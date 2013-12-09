cbd5622ea2f190725163a3c38f7a042b0d83c036
Epthymis <TPrice107@gmail.com>
2013-12-09 01:59:42 -0600

    Printing even more

--- rico.rb
+++ rico.rb
@@ -236,17 +236,22 @@ if $0 == __FILE__  # TYPE OUT A FILE NAME DUMBASS - that's for me.. because I'm 
   min_covering = $stdin.gets.chomp!.to_i
 	### If they are in range then add them d_attributes
 	d_attributes = attr_num_da### The set of decision attribute indexes (ints)
-	p d_attributes #######################################################################################################
+	puts 'Print out the list of decision attributes.'
+  p d_attributes #######################################################################################################
 	attribute_values = Array.new(rel.attributes.length, Array.new) ### Make this# An array that holds the nominal values for each attribute in a subarray
   (0...rel.attributes.length).each do |i|
+    puts "Generating values for attribute #{i}"
     rel.instances.each{|inst|
       attribute_values[i].push(inst[i])
     }
     attribute_values[i] = attribute_values[i].uniq # Removes all duplicate values
-   end
+    puts "Generated values: #{attribute_values[i]}"
+  end
+  puts 'Print out the attribute value table:'
   p attribute_values ###################################################################################################
  	da_partition = partition(rel, d_attributes, attribute_values) # The partition of the decision attributes
-	p da_partition # print to an array ###################################################################################
+	puts 'Print out the partition of decision attributes'
+  p da_partition # print to an array ###################################################################################
   nd_attributes = (0...rel.attributes.length).map{|idx| ### I think that this should just be a list, not a map
 		idx if d_attributes.include?(idx)
 		(idx)}.compact
